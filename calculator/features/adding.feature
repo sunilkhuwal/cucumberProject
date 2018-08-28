d5d70f55992c1f0b516055803b24e54466867439
khuwals <sunil.khuwal@gmail.com>
2018-08-28 23:08:42 +0530

    code

--- calculator/features/adding.feature
+++ calculator/features/adding.feature
@@ -4,4 +4,4 @@ Feature: Adding
     Given the input "2+2"
     When the calculator is run
     Then the output should be "4"
- 
+
--- calculator/features/step_definitions/calculator_steps.rb
+++ calculator/features/step_definitions/calculator_steps.rb
@@ -1,12 +1,13 @@
-Given("the input {string}") do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+Given("the input {string}") do |input|
+  @input = input
 end
 
 When("the calculator is run") do
-  pending # Write code here that turns the phrase above into concrete actions
+  @output = `ruby calc.rb #{@input}`
+  raise('Command failed!') unless $?.success?
 end
 
 Then("the output should be {string}") do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+  @output.should==string
 end
 
