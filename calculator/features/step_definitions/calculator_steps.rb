5cea2eb32e099d11a818c523efdc1a3d7bde021f
khuwals <sunil.khuwal@gmail.com>
2018-08-28 22:42:30 +0530

    added ruby file

---
+++ calculator/features/step_definitions/calculator_steps.rb
@@ -0,0 +1,12 @@
+Given("the input {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("the calculator is run") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("the output should be {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
